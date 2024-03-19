55efbf597a8613d3258d510361310c9ed9e8bce1
LucaOster <Developer0505@outlook.com>
2024-03-20 00:34:32 +0900

    vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Link, Routes, Route} from 'react-router-dom';
+
+import '@styles/App.scss';
+
+function App() {
+  return (
+    <div className='app'>
+    </div>
+  );
+}
+
+export default App;
